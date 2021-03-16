b413f3c97f412c695f7ab0c676b16b127564f44b
BradleyHubbs <75264076+BradleyHubbs@users.noreply.github.com>
2021-03-16 17:53:24 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   const state = {
     additionalPrice: 0,
     car: {
-      price: 26395,
+      price: 26396,
       name: '2019 Ford Mustang',
       image:
         'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
